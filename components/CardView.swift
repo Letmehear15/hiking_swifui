e95bd9b880848689ff75c2634ccf809b451f26b5
alex <sanya.nesterov666@gmail.com>
2024-08-20 12:58:09 +0200

    feat: add reusable button, some logic on click, animation when images change

--- components/CardView.swift
+++ components/CardView.swift
@@ -8,9 +8,21 @@
 import SwiftUI
 
 struct CardView: View {
+    
+    @State private var imageNumber = 1
+    @State private var randomImageNumber = 1
+    
+    
+    func setRandomImage() -> Void {
+        repeat {
+            randomImageNumber = Int.random(in: 1...5)
+        } while(randomImageNumber == imageNumber)
+        
+        imageNumber = randomImageNumber
+        
+    }
+    
     var body: some View {
-        
-        
         ZStack {
             BackgroundView()
             
@@ -30,11 +42,30 @@ struct CardView: View {
                             )
                         )
                         .frame(width: 256, height: 256)
-                    Image("image-1")
+                    Image("image-\(imageNumber)")
                         .resizable()
                         .scaledToFit()
+                        .animation(.smooth(duration: 0.3) ,value: imageNumber)
                 }
+                
+                Button(action: setRandomImage, label: {
+                    Text("Explore more ")
+                        .font(.title2)
+                        .fontWeight(.heavy)
+                        .foregroundStyle(
+                            LinearGradient(
+                                colors:[.CustomColorGreenLight, .CustomColorGreenMedium],
+                                startPoint: .top,
+                                endPoint: .bottom
+                            )
+                        )
+                        .shadow(
+                            color: /*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/.opacity(0.5), radius: 3, x: 1, y: 1
+                        )
+                })
+                .buttonStyle(CustomButtonStyles())
             }
+            
         }.frame(width: 320, height: 580)
         
     }
--- utils/CustomButtonStyles.swift
+++ utils/CustomButtonStyles.swift
@@ -10,6 +10,24 @@ import SwiftUI
 
 struct CustomButtonStyles: ButtonStyle {
     func makeBody(configuration: Configuration) -> some View {
-        configuration.label
+        configuration
+            .label
+            .padding(.vertical)
+            .padding(.horizontal, 40)
+            .background(
+                configuration.isPressed 
+                ? LinearGradient(
+                    colors: [.CustomColorGrayMedium, .CustomColorGrayLight],
+                    startPoint: .top,
+                    endPoint: .bottom
+                )
+                : LinearGradient(
+                    colors: [.CustomColorGrayLight, .CustomColorGrayMedium],
+                    startPoint: .top,
+                    endPoint: .bottom
+                )
+            )
+            .cornerRadius(40)
+            .shadow(color: /*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/.opacity(configuration.isPressed ? 1 : 0.8), radius: 2, x: 3, y: 1)
     }
 }
